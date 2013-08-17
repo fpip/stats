aa3115fd651c2dbaedcb76935345f771538c62c4
Mike Pirnat <mpirnat@gmail.com>
2013-08-17 12:05:28 -0400

    Use the right log file container (derp)

--- download_logs.py
+++ download_logs.py
@@ -10,7 +10,7 @@ pyrax.set_credential_file(creds)
 pyrax.authenticate()
 
 cf_ord = pyrax.connect_to_cloudfiles(region="ORD")
-container = cf_ord.get_container(".ACCESS_LOGS")
+container = cf_ord.get_container(".CDN_ACCESS_LOGS")
 objects = container.get_objects()
 
 for obj in objects:
