29f247e13b6cdcab3a22ec7840d57b85e5e83fed
Mike Pirnat <mpirnat@gmail.com>
2014-01-09 13:55:19 -0500

    More debugging...

--- download_logs.py
+++ download_logs.py
@@ -8,14 +8,24 @@ logs = home + "/logs/cdn"
 
 pyrax.set_setting("identity_type", "rackspace")
 pyrax.set_credential_file(creds, region="ORD")
+print "Authenticating"...
 pyrax.authenticate()
 
 cf = pyrax.cloudfiles
+print "Getting container..."
 container = cf.get_container(".CDN_ACCESS_LOGS")
+print "Getting objects..."
 objects = container.get_objects()
 
+print "Found %s objects to download" % len(objects)
+
 for obj in objects:
-    obj.download(logs, structure=False)
+    try:
+        obj.download(logs, structure=False)
+    except NoSuchObject, e:
+        print "Couldn't download %s; %s" % (obj, e)
+        continue
+
     try:
         obj.delete()
     except NoSuchObject, e:
